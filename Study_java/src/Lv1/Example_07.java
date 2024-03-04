10abc7a195f8c6ac0b56d907337b96e2852cbbb1
liin <gkdldu4@naver.com>
2024-03-04 18:49:26 +0900

    20240304

---
+++ Study_java/src/Lv1/Example_07.java
@@ -0,0 +1,19 @@
+package Lv1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//피보나치 수열이란, 첫 번째 항의 값이 0이고 두 번째 하으이 값이 1일 때, 이후의 항들은 이전의 두 항을 더한 값으로 이루어 지는 수열을 말한다.
+//예) 0,1,1,2,3,5,8,13
+//인풋으로 정수 n으로 받았을때, n이하까지의 파보나치 수열을 출력하는 프로그램을 작성하세요
+public class Example_07 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt(); // 1>0 1 1(3)2>0 1 1 2(4)   3> 0 1 1 2 3 (5)      4> 0 1 1 2 3 (5)   5> 0 1 1 2 3 5(6)  6>0 1 1 2 3 5(6) 7>0 1 1 2 3 5(6) 8>0 1 1 2 3 5 8(7)
+		
+	}
+	
+
+}
